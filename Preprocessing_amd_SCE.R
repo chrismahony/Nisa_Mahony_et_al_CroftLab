0ef62231323595cecad79876644dc78cc9e5f36a
mahonyc <65592074+chrismahony@users.noreply.github.com>
2023-11-29 15:33:39 +0000

    Update and rename Preprocessing_amd_SCE.R to Preprocessing_and_SCE.R

--- Preprocessing_amd_SCE.R
+++
@@ -1,1 +0,0 @@
-Patritcia to add
---
+++ Preprocessing_and_SCE.R
@@ -0,0 +1,4 @@
+#Patritcia can you add the script you ran ofr building the initial visiuim object?
+#Reading in data, creating seuart objects, merging data preprocessing, clustering etc.
+#Thanks!
+
