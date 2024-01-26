eeaebf0f00c8033f6ef48182ecfcaeb1fc7dc508
mahonyc <65592074+chrismahony@users.noreply.github.com>
2024-01-26 15:27:18 +0000

    Update processing_hum0207_bulkRNA.R

--- processing_hum0207_bulkRNA.R
+++ processing_hum0207_bulkRNA.R
@@ -1,3 +1,4 @@
+
 hg19.v27.samplefiltered <- read.delim("/rds/projects/c/croftap-runx1data01/analysis_of_public_data_sets/Bulk_human_data/hum0207_RNAseq/hum0207.v1.RNA.v1/RNAseq/hg19.v27.samplefiltered.htseq")
 
 hg19.v27.samplefiltered<-hg19.v27.samplefiltered  %>% distinct(gname, .keep_all = TRUE)
@@ -27,71 +28,51 @@ dds <- DESeqDataSetFromMatrix(countData=hg19.v27.samplefiltered_all,
                               colData=meta_data_dds, 
                               design=~group, tidy = F)
 
-keep <- rowSums(counts(dds)) >= 10
-dds <- dds[keep,]
-
-
 dds$group <- relevel(dds$group, ref = "NS")
 
-dds <- DESeq(dds)
+#mingenecount <- quantile(rowSums(counts(dds)), 0.5)
+mingenecount <- 200
+maxgenecount <- quantile(rowSums(counts(dds)), 0.999)
+dim(counts(dds))
+# Subset low-expressed genes
+keep <- rowSums(counts(dds)) > mingenecount & rowSums(counts(dds)) < maxgenecount
+dds <- dds[keep, ]
+print(quantile(rowSums(counts(dds))))
+dim(dds)
 
-dds <- estimateSizeFactors(dds)
-sizeFactors(dds)
+dds@colData[['group']] <- as.factor(dds@colData[['group']])
+
+design(dds) <- formula(~ group)
+print(design(dds))
+dds <- DESeq(dds, test = "Wald")
 
 
-deseq2Results <- results(dds)
-deseq2ResDF <- as.data.frame(deseq2Results)
-summary(deseq2Results)
-deseq2ResDF$significant <- ifelse(deseq2ResDF$padj < .1, "Significant", NA)
-#strict
-sigGenes <- rownames(deseq2ResDF[deseq2ResDF$padj <= .01,])
+targetvar <- "group"
+
+#comps1 <- data.frame(t(combn(unique(as.character(meta_data_dds[[targetvar]])), 2)))
+     # head(comps1)
+      
+      comps1 <- data.frame(X1=unique(meta.data$V2)[-c(6,11)] ,X2= rep("NS",9))      
+
+      
+      ress <- apply(comps1, 1, function(cp) {
+        print(cp)
+        res <- data.frame(results(dds, contrast=c(targetvar, cp[1], cp[2])))
+        res[["gene"]] <- rownames(res)
+        res[["comparison"]] <- paste0(cp[1], "_vs_", cp[2])
+        res
+      })
+
+     
+            
+      res1 <- Reduce(rbind, ress)
+
+res1 %>% 
+      filter(padj < 0.05) %>%
+      mutate('score' = log2FoldChange*(-log10(pvalue))) %>%
+      arrange(desc(abs(score))) -> subres
+
+for (i in 1:length(unique(res1$comparison))){res1 %>% filter(comparison == unique(res1$comparison)[[i]] & padj < 0.05 & log2FoldChange > 2) %>% write.csv(paste0("/rds/projects/c/croftap-visium-manuscript-01/Visium_CManalysis/paper_figures/", unique(res1$comparison)[[i]], "_new.csv"))}
 
 
-deseq2VST <- vst(dds)
-deseq2VST <- assay(deseq2VST)
-deseq2VST <- as.data.frame(deseq2VST)
-deseq2VST$Gene <- rownames(deseq2VST)
-deseq2VST <- deseq2VST[deseq2VST$Gene %in% sigGenes,]
-
-library(ComplexHeatmap)
-Heatmap(deseq2VST,cluster_columns =F)
-
-library(DESeq2)
-resultsNames(dds)
-IL6_vs_NS <- lfcShrink(dds, coef="group_IL6_vs_NS", type="apeglm")
-IL6_vs_NS<-as.data.frame(IL6_vs_NS)
-IL6_vs_NS$gene<-rownames(IL6_vs_NS)
-IL6_vs_NS<-IL6_vs_NS[order(IL6_vs_NS$padj),]
-#IL6_vs_NS<-IL6_vs_NS[IL6_vs_NS$log2FoldChange > 0.2,]
-#write.csv(IL6_vs_NS, "/rds/projects/c/croftap-visium-manuscript-01/Visium_CManalysis/IL6_vs_NS.csv")
-
-#write.csv(IL6_vs_NS, "/rds/projects/c/croftap-visium-manuscript-01/Visium_CManalysis/zone_annotationIL6_vs_NS_sorted.csv")
-
-TNFa_vs_NS <- lfcShrink(dds, coef="group_TNFa_vs_NS", type="apeglm")
-TNFa_vs_NS<-as.data.frame(TNFa_vs_NS)
-TNFa_vs_NS$gene<-rownames(TNFa_vs_NS)
-TNFa_vs_NS<-TNFa_vs_NS[order(TNFa_vs_NS$padj),]
-#TNFa_vs_NS<-TNFa_vs_NS[TNFa_vs_NS$log2FoldChange > 0.2,]
-#write.csv(TNFa_vs_NS, "/rds/projects/c/croftap-visium-manuscript-01/Visium_CManalysis/TNFa_vs_NS.csv")
-
-TGFb_vs_NS <- lfcShrink(dds, coef="group_TGFb_vs_NS", type="apeglm")
-TGFb_vs_NS<-as.data.frame(TGFb_vs_NS)
-TGFb_vs_NS$gene<-rownames(TGFb_vs_NS)
-TGFb_vs_NS<-TGFb_vs_NS[order(TGFb_vs_NS$padj),]
-TGFb_vs_NS<-TGFb_vs_NS[TGFb_vs_NS$log2FoldChange > 0.2,]
-write.csv(TGFb_vs_NS, "/rds/projects/c/croftap-visium-manuscript-01/Visium_CManalysis/TGFb_vs_NS.csv")
-
-IFNg_vs_NS <- lfcShrink(dds, coef="group_IFNg_vs_NS", type="apeglm")
-IFNg_vs_NS<-as.data.frame(IFNg_vs_NS)
-IFNg_vs_NS$gene<-rownames(IFNg_vs_NS)
-IFNg_vs_NS<-IFNg_vs_NS[order(IFNg_vs_NS$padj),]
-IFNg_vs_NS<-IFNg_vs_NS[IFNg_vs_NS$log2FoldChange > 0.2,]
-write.csv(IFNg_vs_NS, "/rds/projects/c/croftap-visium-manuscript-01/Visium_CManalysis/IFNg_vs_NS.csv")
-
-IL1b_vs_NS <- lfcShrink(dds, coef="group_IL1b_vs_NS", type="apeglm")
-IL1b_vs_NS<-as.data.frame(IL1b_vs_NS)
-IL1b_vs_NS$gene<-rownames(IL1b_vs_NS)
-IL1b_vs_NS<-IL1b_vs_NS[order(IL1b_vs_NS$padj),]
-IL1b_vs_NS<-IL1b_vs_NS[IL1b_vs_NS$log2FoldChange > 0.2,]
-write.csv(IL1b_vs_NS, "/rds/projects/c/croftap-visium-manuscript-01/Visium_CManalysis/IL1b_vs_NS.csv")
 
